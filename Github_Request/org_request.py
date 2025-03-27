7e480ba74435d47f356c6017709d21eac6e0911b
Nicolas Daza Hernandez <ndaza@adgasd32.int>
2025-03-27 11:56:07 -0500

    cambios para obtener todos los datos en la requests

--- Github_Request/org_request.py
+++ Github_Request/org_request.py
@@ -13,7 +13,8 @@ class GithubOrgAPI:
     def get_org_repos(org_name: str): #Obtiene los repositorios de una organización de GitHub.
         url = f"{GithubOrgAPI.BASE_URL}/orgs/{org_name}/repos"
         headers = {"Authorization": f"Bearer {GithubOrgAPI.TOKEN}"}#Se establece la autorización a traves de token
-        response = requests.get(url, headers=headers)
+        params ={ "type": "all", "per_page": 100}
+        response = requests.get(url, headers=headers, params=params)
         if response.status_code == 200:
             return response.json()
         response.raise_for_status()# Lanza una HTTPError si la solicitud a la API de GitHub falla.
