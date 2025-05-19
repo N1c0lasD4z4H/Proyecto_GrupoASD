d7df42c65dffd7106e7cf61a84ed1cd11cf52498
Nicolas Daza Hernandez <ndaza@grupoasd.com>
2025-05-19 17:54:53 -0500

    feat: se agreaga la función de envio de documents en el router

--- Elastic/elastic_client.py
+++ Elastic/elastic_client.py
@@ -1,16 +1,15 @@
-from elasticsearch import AsyncElasticsearch
+from elasticsearch import Elasticsearch  # ✅ usa cliente síncrono
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-es = AsyncElasticsearch(
+es = Elasticsearch(
     hosts=[os.getenv("ELASTICSEARCH_URL")], 
     basic_auth=(
         os.getenv("ELASTICSEARCH_USER"),
         os.getenv("ELASTICSEARCH_PASSWORD")
     ),
-    ca_certs="C:\ELK\elasticsearch\config\certs\http_ca.crt",
+    ca_certs="C:\\ELK\\elasticsearch\\config\\certs\\http_ca.crt",
     verify_certs=True
 )
-
